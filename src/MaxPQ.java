0e2c4f77e0c7c51a96cfae2076a00157c1190a72
Havret <h4vret@gmail.com>
2019-04-01 22:58:00 +0200

    Implement Resizing for MaxPQ

--- src/MaxPQ.java
+++ src/MaxPQ.java
@@ -1,23 +1,33 @@
 public class MaxPQ<Key extends Comparable<Key>> {
     private Key[] pq;
-    private int N = 0;
+    private int n = 0;
 
     public MaxPQ(int maxN) {
         pq = (Key[]) new Comparable[maxN + 1];
     }
 
+    public MaxPQ() {
+        this(1);
+    }
+
     public boolean isEmpty() {
-        return N == 0;
+        return n == 0;
     }
 
     public int size() {
-        return N;
+        return n;
     }
 
     public void insert(Key v) {
-        pq[++N] = v;
-        swim(N);
+        // double size of array if necessary
+        if (n == pq.length - 1) {
+            resize(2 * pq.length);
+        }
+
+        pq[++n] = v;
+        swim(n);
     }
+
 
     private void swim(int k) {
         while (k > 1 && less(k / 2, k)) {
@@ -28,16 +38,30 @@ public class MaxPQ<Key extends Comparable<Key>> {
 
     public Key delMax() {
         Key max = pq[1];
-        exch(1, N--);
-        pq[N + 1] = null;
+        exch(1, n--);
+        pq[n + 1] = null;
         sink(1);
+
+        if ((n > 0) && n == (pq.length - 1) / 4) {
+            resize(pq.length / 2);
+        }
+
         return max;
     }
 
+    private void resize(int capacity) {
+        Key[] temp = (Key[]) new Comparable[capacity];
+        for (int i = 0; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+
+
     private void sink(int k) {
-        while (2 * k <= N) {
+        while (2 * k <= n) {
             int j = 2 * k;
-            if (j < N && less(j, j + 1)) {
+            if (j < n && less(j, j + 1)) {
                 j++;
             }
             if (!less(k, j)) {
--- test/MaxPQTest.java
+++ test/MaxPQTest.java
@@ -21,4 +21,23 @@ class MaxPQTest {
             assertEquals(i, maxPQ.delMax());
         }
     }
+
+    @Test
+    void should_process_elements_in_descending_order_when_pq_initialized_without_given_capacity() {
+        var maxPQ = new MaxPQ<Integer>();
+        maxPQ.insert(1);
+        maxPQ.insert(9);
+        maxPQ.insert(2);
+        maxPQ.insert(8);
+        maxPQ.insert(3);
+        maxPQ.insert(7);
+        maxPQ.insert(4);
+        maxPQ.insert(6);
+        maxPQ.insert(5);
+        maxPQ.insert(10);
+
+        for (int i = 10; i > 0; i--) {
+            assertEquals(i, maxPQ.delMax());
+        }
+    }
 }
