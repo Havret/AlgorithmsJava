989259e8a8b0f32963ec1cc782321021520da4be
Havret <h4vret@gmail.com>
2019-04-30 15:17:47 +0200

    Most-significant-digit-first (MSD) string sort implementation

--- src/MSD.java
+++ src/MSD.java
@@ -1,6 +1,9 @@
+/**
+ * Most-significant-digit-first (MSD) string sort implementation
+ */
 public class MSD {
     private static int R = 256;
-    private static final int M = 15;
+    private static final int M = 10;
     private static String[] aux;
 
     public static void sort(String[] a) {
@@ -13,34 +16,34 @@ public class MSD {
     private static void sort(String[] a, int lo, int hi, int d) {
         if (hi <= lo + M) {
             insertion(a, lo, hi, d);
+            return;
         }
 
+        // compute frequency counts
         int[] count = new int[R + 2];
-
-        // Compute frequency counts.
         for (int i = lo; i <= hi; i++) {
-            count[charAt(a[i], d) + 2]++;
+            int c = charAt(a[i], d);
+            count[c + 2]++;
         }
 
-        // Transform counts to indices.
-        for (int r = 0; r < R + 1; r++) {
+        // transform counts to indicies
+        for (int r = 0; r < R + 1; r++)
             count[r + 1] += count[r];
+
+        // distribute
+        for (int i = lo; i <= hi; i++) {
+            int c = charAt(a[i], d);
+            aux[count[c + 1]++] = a[i];
         }
 
-        // Distribute.
-        for (int i = lo; i <= hi; i++) {
-            aux[count[charAt(a[i], d) + 1]++] = a[i];
-        }
-
-        // Copy back
-        for (int i = lo; i <= hi; i++) {
+        // copy back
+        for (int i = lo; i <= hi; i++)
             a[i] = aux[i - lo];
-        }
 
-        // Recursively sort for each character value.
-        for (int r = 0; r < R; r++) {
+
+        // recursively sort for each character (excludes sentinel -1)
+        for (int r = 0; r < R; r++)
             sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
-        }
     }
 
     // insertion sort a[lo..hi], starting at dth character
@@ -66,7 +69,8 @@ public class MSD {
     }
 
     private static int charAt(String s, int d) {
-        if (d < s.length()) return s.charAt(d);
-        else return -1;
+        assert d >= 0 && d <= s.length();
+        if (d == s.length()) return -1;
+        return s.charAt(d);
     }
 }
--- test/MSDTest.java
+++ test/MSDTest.java
@@ -6,21 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MSDTest {
     @Test
-    void it_should_sort_array_of_fixed_length_strings() {
+    void it_should_sort_array_of_strings() {
         String[] a = {
-                "4PGC938",
-                "2IYE230",
-                "3CIO720",
-                "1ICK750",
-                "1OHV845",
-                "4JZY524",
-                "1ICK750",
-                "3CIO720",
-                "1OHV845",
-                "1OHV845",
-                "2RLA629",
-                "2RLA629",
-                "3ATW723",
+                "she",
+                "sells",
+                "seashells",
+                "by",
+                "the",
+                "sea",
+                "shore",
+                "shells",
+                "she",
+                "sells",
+                "are",
+                "surely",
+                "seashells",
         };
 
         MSD.sort(a);
