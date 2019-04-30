9ed6c3e8a5bfb629b016526cf23dd8bb75b6e331
Havret <h4vret@gmail.com>
2019-04-30 20:07:58 +0200

    2-sum problem no need to sort array

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Class to determine whether there are two distinct integer i and i in an array a
  * of n 64-bit integers such that a[i] + a[j] = T
@@ -7,20 +9,14 @@ public class TwoSum {
     }
 
     public static boolean sumExists(long[] a, long T) {
-        LSD.sort(a);
-
-        int i = 0, j = a.length - 1;
-
-        while (i != j) {
-            long sum = a[i] + a[j];
-            if (sum == T) {
+        HashSet<Long> complementSet = new HashSet<>();
+        for (int i = 0; i < a.length; i++) {
+            long current = a[i];
+            if (complementSet.contains(current)) {
                 return true;
-            }
-
-            if (sum < T) {
-                i++;
-            } else if (sum > T) {
-                j--;
+            } else {
+                long complement = T - current;
+                complementSet.add(complement);
             }
         }
 
